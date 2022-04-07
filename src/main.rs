578541bd6108c8fe458d4f36a7b574abb3d37d8a
merelymyself <poonsters6@gmail.com>
2022-04-07 13:43:40 +0800

    the coder quickly collapses instead of the coder quickly collapse

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::path::Path;
 use std::io::Error;
+use std::env;
 
 fn count_lines_better(filename:&str) -> i32 {
 	let file: BufReader<File> = BufReader::new(File::open(filename).expect("Unable to open file"));
@@ -28,7 +29,14 @@ fn get_word(firstletter:char) -> String {
 	let txtarray: [&str; 11] = ["./txt/nouns.txt", "./txt/singularnouns.txt", "./txt/pluralnouns.txt", "./txt/opinionadjectives.txt", "./txt/sizeadjectives.txt", "./txt/ageadjectives.txt", "./txt/colouradjectives.txt", "./txt/materialadjectives.txt", "./txt/transitiveverbs.txt", "./txt/intransitiveverbs.txt", "./txt/adverbs.txt"];
 	let mut wordtype: usize = 0;
 	if firstletter == 't' {
-		String::from("the")
+		return String::from("the")
+	}
+	else if firstletter == 'I' {
+		let mut rng = rand::thread_rng();
+		let outputnumber = rng.gen_range(1..(count_lines_better("./txt/intransitiveverbs.txt")));
+		let mut tempword = get_line_at(Path::new("./txt/intransitiveverbs.txt"),outputnumber as usize).expect("Error!");
+		tempword.push('s');
+		return tempword;
 	}
 	else {
 		for n in 0..11 {
@@ -50,6 +58,7 @@ fn get_structure(adjectives:i32, adverbs:i32) -> [char;15] {
 		structarray[0] = 't';
 		cnt = cnt + 1;
 	}
+	let mut plural:bool = true;
 	let mut cnt2 = adjectives;
 	let adjarray = ['O','S','A','C','M'];
 	let mut adjarray2 = ['X';5];
@@ -68,8 +77,10 @@ fn get_structure(adjectives:i32, adverbs:i32) -> [char;15] {
 	if structarray[0]=='t'{
 		if rng.gen_range(1..3) == 1{
 			structarray[cnt as usize]='p';
+			plural = true;
 		} else {
 			structarray[cnt as usize]='s';
+			plural = false;
 		}
 	}
 	else {
@@ -80,12 +91,20 @@ fn get_structure(adjectives:i32, adverbs:i32) -> [char;15] {
 		structarray[cnt as usize]='a';
 		cnt = cnt + 1;
 	}
-	structarray[cnt as usize]='i';
+	if plural == true {
+		structarray[cnt as usize]='i';
+	}
+	else {
+		structarray[cnt as usize] = 'I';
+	}
 	structarray
 }
-//let ch = text.chars().nth(0).unwrap();
 
 fn main() {
+	let arguments: Vec<String> = env::args().collect();
+	if arguments[0] == "h"{
+
+	}
 	let y = get_structure(2, 1);
 	let y2: String = y.iter().collect();
 	println!("{}", y2);
@@ -97,5 +116,5 @@ fn main() {
 			print!("{} ", get_word(letter));
 		}
 	}
+	print!("\nType in 'cgisf h' for help.")
 }
-
